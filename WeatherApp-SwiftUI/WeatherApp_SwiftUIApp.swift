c376fa9abbdb63d1143f56be0bba809ea1b7cd6a
Jakaria Noman <jnomann004@gmail.com>
2025-01-19 20:02:47 +0600

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Jakaria Noman on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
